6acd206392b82a855c8e648a9f9fe22e897050ff
Mariogc55 <mariogc5555@gmail.com>
2025-11-23 14:25:04 -0500

    icons-update

--- frontend-vue/src/views/ChatbotView.vue
+++ frontend-vue/src/views/ChatbotView.vue
@@ -22,7 +22,7 @@
 
       <nav class="p-4 space-y-2">
         <a href="https://jiratmario-1761276751230.atlassian.net/jira/software/projects/KAN/list" target="_blank" class="flex items-center p-2 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-blue-400 transition duration-150 whitespace-nowrap">
-          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2m-9 0V3h4v2m-4 0h4"></path></svg>
+          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
           <span v-if="isSidebarOpen" class="ml-3">Tareas Jira</span>
         </a>
 
@@ -32,7 +32,7 @@
         </a>
 
         <router-link to="/reports" class="flex items-center p-2 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-blue-400 transition duration-150 whitespace-nowrap">
-          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0h2m-2 0h-2m4 0h4m-4 0v-6a2 2 0 012-2h2a2 2 0 012 2v6a2 2 0 01-2 2h-2a2 2 0 01-2-2zm4-6h2m-4 0h-2m4 0h4"></path></svg>
+          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
           <span v-if="isSidebarOpen" class="ml-3">Reportes SinergIA</span>
         </router-link>
 
